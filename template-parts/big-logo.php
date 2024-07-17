0afc7ed57267bd953d310e11764c2f4e3594648d
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-07-17 16:41:42 +0300

    add logo template

---
+++ template-parts/big-logo.php
@@ -0,0 +1,11 @@
+<?php
+$ds_big_logo_url = $args['url'];
+?>
+
+<figure class="flex mx-auto mt-0 mb-9.5">
+    <img
+            src="<?php echo $ds_big_logo_url; ?>"
+            alt="<?php echo get_bloginfo('title'); ?>"
+            style="max-width: 252px;"
+    >
+</figure>
