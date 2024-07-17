c6a33b52cd3a8b7e258e9984af8bba7a43b007b0
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-07-17 16:42:20 +0300

    create service card

---
+++ template-parts/service-card.php
@@ -0,0 +1,28 @@
+<?php
+$ds_service_data = $args['service'];
+?>
+<div class="masonry-item text-white">
+    <div class="info-block-inner-block">
+        <div class="flex items-center gap-x-2.5 py-1.5 mb-3">
+            <?php if (!empty($ds_service_data['icon_url'])) : ?>
+                <div class="">
+                    <img src="<?php echo $ds_service_data['icon_url']; ?>"
+                         alt="<?php echo $ds_service_data['title']; ?>">
+                </div>
+            <?php endif; ?>
+            <p class="text-white text-22 leading-1.2 font-bold">
+                <?php echo $ds_service_data['title']; ?>
+            </p>
+        </div>
+        <?php if (get_the_excerpt($ds_service_data['id'])) : ?>
+            <p class="mb-3 text-[17px] leading-[27px]">
+                <?php echo get_the_excerpt($ds_service_data['id']); ?>
+            </p>
+        <?php endif; ?>
+
+        <a href="<?php echo $ds_service_data['permalink']; ?>"
+           class="info-block-inner-block-btn">
+            Подробно об услуге
+        </a>
+    </div>
+</div>
