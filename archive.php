e5c068bc3a8b523657fcc21a10f7f4b1972b9f03
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-07-17 22:31:29 +0300

    create archive

--- archive.php
+++ archive.php
@@ -1,51 +1,34 @@
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Deciders
- */
+$ds_theme_settings = get_option('theme_settings');
+$ds_unified_content = ds_get_unified_post_types_array(array(
+    'exclude_posts' => array('post', 'service')
+));
 
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main class="grid grid-cols-[1fr_3fr] gap-x-6 items-start relative z-10">
+        <div class="">
+            <?php
+            get_template_part('/template-parts/big-logo', null, array(
+                'url' => $ds_theme_settings['logo'],
+            ));
 
-		<?php if ( have_posts() ) : ?>
+            get_template_part('template-parts/service-list');
+            ?>
+        </div>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+        <div>
+            <div class="layout-grid layout-3">
+                <div class="grid-sizer"></div>
+                <?php
+                foreach ($ds_unified_content as $ds_unified_content_item) {
+                    get_template_part('/template-parts/post-card', null, ['post' => $ds_unified_content_item]);
+                }
+                ?>
+            </div>
+        </div>
+	</main>
 
 <?php
-get_sidebar();
 get_footer();
