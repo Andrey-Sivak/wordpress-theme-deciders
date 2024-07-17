414952a468c3080d9b708d7254a0a293e86f9597
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-07-17 22:14:57 +0300

    clear footer

--- footer.php
+++ footer.php
@@ -11,22 +11,7 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ds' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ds' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ds' ), 'ds', '<a href="https://github.com/WEBzaytsev">WEBzaytsev</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer id="colophon" class="site-footer"></footer>
 
 <?php wp_footer(); ?>
 
