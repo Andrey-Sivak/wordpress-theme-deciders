986f521fe23199011801412b978675e544305072
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-07-17 16:53:11 +0300

    style and script for masonry grid

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
-console.log(1)
+jQuery(document).ready(function($) {
+    var grid = document.querySelector('.layout-grid');
+
+    var msnry = new Masonry( grid, {
+        itemSelector: '.masonry-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+        gutter: 22
+    });
+
+    imagesLoaded( grid ).on( 'progress', function() {
+        // layout Masonry after each image loads
+        msnry.layout();
+    });
+});
