b01a8a7392cf132150dc904e93470babfc0aede7
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-08-08 13:50:55 +0300

    refactor: use base post loop in the archive

--- archive.php
+++ archive.php
@@ -1,12 +1,9 @@
 <?php
 $ds_theme_settings = get_option('theme_settings');
 
-$ds_cases = get_posts(array(
-    'post_type' => 'case',
-    'post_status' => 'publish',
-    'posts_per_page' => -1,
+$ds_cases = ds_get_unified_post_types_array(array(
+    'exclude_posts' => array('service', 'post')
 ));
-
 get_header();
 ?>
 
@@ -29,21 +26,12 @@ get_header();
             <div class="layout-grid layout-3" id="cases-list">
                 <div class="grid-sizer"></div>
                 <?php
-                foreach ($ds_cases as $ds_case) : ?>
-                    <a href="<?php echo get_permalink($ds_case->ID); ?>" class="masonry-item group text-white ds-post md:pb-6.5 pb-0">
-                        <?php
-                        if (has_post_thumbnail($ds_case->ID)) {
-                            echo get_the_post_thumbnail(
-                                $ds_case->ID,
-                                'large',
-                                [
-                                    'class' => 'relative z-20'
-                                ]
-                            );
-                        }
-                        ?>
-                    </a>
-                <?php endforeach; ?>
+                if (!empty($ds_cases)) {
+                    foreach ($ds_cases as $ds_case) {
+                        get_template_part('/template-parts/post-card', null, ['post' => $ds_case]);
+                    }
+                }
+                ?>
             </div>
         </div>
     </main>
