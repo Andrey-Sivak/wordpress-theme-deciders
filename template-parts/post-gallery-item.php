8a763bf4bb7d2ced8213037cc9db3f094e4840d7
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-08-09 12:07:41 +0300

    refactor: edit display for post gallery image

--- template-parts/post-gallery-item.php
+++ template-parts/post-gallery-item.php
@@ -2,14 +2,17 @@
 $ds_post_gallery_item_img_id = $args['img_id'];
 
 if ($ds_post_gallery_item_img_id) :
-    $image_url = wp_get_attachment_image_url($ds_post_gallery_item_img_id, 'large');
-    $image_alt = get_post_meta($ds_post_gallery_item_img_id, '_wp_attachment_image_alt', true);
-    if ($image_url) :
+    ?>
+    <div class="masonry-item ds-post md:block flex items-center justify-center">
+        <?php
+        echo wp_get_attachment_image(
+            $ds_post_gallery_item_img_id,
+            'large',
+            false,
+            [
+                'loading' => 'lazy',
+            ]
+        );
         ?>
-
-        <div class="masonry-item ds-post md:block flex items-center justify-center">
-            <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
-        </div>
-
-    <?php endif;
-endif;
+    </div>
+<?php endif; ?>
