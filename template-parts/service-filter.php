86e5ce55d414f7977473e269154eb07fe6680c5a
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-08-10 15:38:25 +0300

    refactor: edit filter styles

--- template-parts/service-filter.php
+++ template-parts/service-filter.php
@@ -11,10 +11,10 @@ $ds_services = get_posts(array(
 <div class="info-block-inner-block service-filter mb-3" id="service-filter">
     <p class="text-20 leading-1.2 mb-2.5">Фильтр по услугам:</p>
     <ul
-        class="service-grid flex gap-x-6 gap-y-3.5 flex-wrap transition-all duration-300"
+        class="service-grid flex gap-x-1.5 gap-y-3.5 flex-wrap transition-all duration-300"
         id="service-grid"
     >
-        <li class="service-item" data-service-id="all" style="order: 1">
+        <li class="service-item" data-service-id="all">
             <button class="service-filter-btn active flex items-center gap-x-2.5 py-1.5 cursor-pointer">
                 <span><?php get_template_part('/vector-images/icon', 'all-service-filter'); ?></span>
                 <span class="font-bold">Все</span>
@@ -22,12 +22,11 @@ $ds_services = get_posts(array(
         </li>
 
         <?php
-        $ds_order = 2;
         foreach ($ds_services as $ds_service) :
             $ds_icon_id = get_post_meta($ds_service->ID, '_service_icon_id', true);
             $ds_icon_html = $ds_icon_id ? wp_get_attachment_image($ds_icon_id, 'thumbnail', false, array('class' => 'service-icon')) : '';
             ?>
-            <li class="service-item" data-service-id="<?php echo esc_attr($ds_service->ID); ?>" style="order: <?php echo $ds_order; ?>">
+            <li class="service-item" data-service-id="<?php echo esc_attr($ds_service->ID); ?>">
                 <button
                     class="service-filter-btn flex items-center gap-x-2.5 py-1.5 cursor-pointer"
                 >
@@ -37,6 +36,6 @@ $ds_services = get_posts(array(
                     <span class="font-bold"><?php echo $ds_service->post_title; ?></span>
                 </button>
             </li>
-            <?php $ds_order++; endforeach; ?>
+        <?php endforeach; ?>
     </ul>
 </div>
