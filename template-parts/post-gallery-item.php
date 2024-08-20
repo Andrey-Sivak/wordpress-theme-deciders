4840a6b09a0c38e2355d1774639f0a254f24d593
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2024-08-20 15:11:23 +0300

    build: fix case gallery item rounded

--- template-parts/post-gallery-item.php
+++ template-parts/post-gallery-item.php
@@ -3,12 +3,12 @@ $ds_post_gallery_item_img_id = $args['img_id'];
 
 if ($ds_post_gallery_item_img_id) :
     ?>
-    <div class="masonry-item ds-post md:block flex items-center justify-center relative rounded-2xl">
+    <div class="masonry-item ds-post md:block flex items-center justify-center relative">
         <?php
         get_template_part('/vector-images/image-loader', null, array('color' => '#63A2ED'));
         get_template_part('/template-parts/advanced-image', null, array(
             'img_id' => $ds_post_gallery_item_img_id,
-            'class' => 'relative z-20'
+            'class' => 'relative z-20 rounded-2xl'
         ));
         ?>
     </div>
